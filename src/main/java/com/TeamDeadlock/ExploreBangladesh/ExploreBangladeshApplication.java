8b1db74fca0e76fc549e644823906cfbb26e85ba
Adnin Ropun <adninzahid55@gmail.com>
2025-12-15 13:16:44 +0600

    Initialize JAVA SPRING

---
+++ src/main/java/com/TeamDeadlock/ExploreBangladesh/ExploreBangladeshApplication.java
@@ -0,0 +1,13 @@
+package com.TeamDeadlock.ExploreBangladesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreBangladeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreBangladeshApplication.class, args);
+	}
+
+}
